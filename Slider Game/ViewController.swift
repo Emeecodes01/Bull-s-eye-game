3c406e3ee02fd6ee1826204b40884789cc5746a9
Emmanuel Ozibo <eozibo@appzonegroup.com>
2020-01-21 08:56:11 +0100

    Initial Commit

---
+++ Slider Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider Game
+//
+//  Created by Emmanuel Mac on 21/01/2020.
+//  Copyright © 2020 Emmanuel Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
